356cffce28bd17d6555a90ff54479bc61659201c
Roman Havrylko <groman2000@gmail.com>
2024-08-31 18:02:53 +0300

    feat: added app params

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v1');
+  app.enableCors();
+  app.enableShutdownHooks();
   await app.listen(3000);
 }
 bootstrap();
