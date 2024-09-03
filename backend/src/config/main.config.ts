13a6e9c164fcebcead800ae2390878a10957aeb4
Roman Havrylko <groman2000@gmail.com>
2024-09-03 17:01:29 +0300

    refactor: moved constant vals  to env

---
+++ backend/src/config/main.config.ts
@@ -0,0 +1,5 @@
+export const mainConfig = {
+  frontendURI: process.env.FRONTEND_URI,
+  apiPrefix: process.env.API_PREFIX || 'api/v1',
+  apiPort: process.env.API_PORT || 3001,
+};
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,21 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
+import { mainConfig } from './config/main.config';
+
+const { frontendURI, apiPrefix, apiPort } = mainConfig;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api/v1');
-  app.enableCors();
+  app.setGlobalPrefix(apiPrefix);
+  app.enableCors({
+    origin: frontendURI,
+    credentials: true,
+  });
   app.enableShutdownHooks();
-  await app.listen(3001);
+  app.use(cookieParser());
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(apiPort);
 }
 bootstrap();
