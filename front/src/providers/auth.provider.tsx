5473af89f547d7774b238589ff30f93e57294c49
Roman Havrylko <groman2000@gmail.com>
2024-09-03 17:11:02 +0300

    feat: added work with refresh token and changed work with access token

--- front/src/providers/auth.provider.tsx
+++ front/src/providers/auth.provider.tsx
@@ -1,57 +1,90 @@
 import { User } from '@app/common/interfaces';
-import { createContext, ReactNode, useContext, useState } from 'react';
+import axios from 'axios';
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
+import { configuration } from '../common/config';
 
-export interface UserData {
-  user: User;
-  accessToken?: string;
-}
-
-interface AuthContextProps {
-  userData: UserData | null;
-  login: (userData: { email: string; password: string }) => Promise<void>;
+interface AuthContextType {
+  user: User | null;
+  accessToken: string | null;
+  login: (email: string, password: string) => Promise<void>;
   logout: () => void;
+  refreshAccessToken: () => Promise<void>;
 }
 
-const AuthContext = createContext<AuthContextProps | undefined>(undefined);
+const { baseApiUri } = configuration;
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
-  const [userData, setUserData] = useState<UserData | null>(null);
-  const login = async (userData: { email: string; password: string }) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [accessToken, setAccessToken] = useState<string | null>(
+    localStorage.getItem('accessToken'),
+  );
+
+  useEffect(() => {
+    if (!accessToken) {
+      localStorage.removeItem('accessToken');
+      return;
+    }
+
+    localStorage.setItem('accessToken', accessToken);
+    axios
+      .get<User>(`${baseApiUri}/api/v1/users/profile`, {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      })
+      .then((response) => {
+        setUser({ ...response.data });
+      })
+      .catch(() => {
+        localStorage.removeItem('accessToken');
+      });
+  }, [accessToken]);
+
+  const login = async (email: string, password: string) => {
     try {
-      const response = await fetch(
-        'http://localhost:3001/api/v1/auth/sign-in',
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(userData),
-        },
-      );
-
-      if (!response.ok) {
-        throw new Error('Failed to log in');
-      }
-
-      const data = await response.json();
-      setUserData(data);
+      const { data } = await axios.post(`${baseApiUri}/api/v1/auth/sign-in`, {
+        email,
+        password,
+      });
+      setAccessToken(data.accessToken);
+      setUser({ ...data.user, token: data.accessToken });
     } catch (error) {
       console.error('Login error:', error);
     }
   };
 
   const logout = () => {
-    setUserData(null);
+    setAccessToken(null);
+    setUser(null);
+  };
+
+  const refreshAccessToken = async () => {
+    try {
+      const response = await axios.post(
+        `${baseApiUri}/api/v1/auth/refresh-token`,
+      );
+      setAccessToken(response.data.accessToken);
+    } catch (error) {
+      console.error('Refresh token error:', error);
+    }
   };
 
   return (
-    <AuthContext.Provider value={{ userData: userData, login, logout }}>
+    <AuthContext.Provider
+      value={{ user, accessToken, login, logout, refreshAccessToken }}
+    >
       {children}
     </AuthContext.Provider>
   );
 };
 
-export const useAuth = (): AuthContextProps => {
+export const useAuth = () => {
   const context = useContext(AuthContext);
   if (!context) {
     throw new Error('useAuth must be used within an AuthProvider');
