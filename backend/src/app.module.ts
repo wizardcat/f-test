5990ef7404d63ff436b8f63e624661bc13bc63f5
Roman Havrylko <groman2000@gmail.com>
2024-09-05 23:47:57 +0300

    feat: imprived main app modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,22 +1,46 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 import databaseConfig from './config/database.config';
+import { CryptoModule } from './crypto/crypto.module';
+import { DatabaseModule } from './database/database.module';
 import { databaseProviders } from './database/database.providers';
 import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [databaseConfig],
       isGlobal: true,
+      cache: true,
     }),
     UsersModule,
     AuthModule,
+    CryptoModule,
+    DatabaseModule,
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10) || 3306,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+    }),
+
   ],
   controllers: [AppController],
-  providers: [AppService, ...databaseProviders],
+  providers: [
+    AppService,
+    ...databaseProviders,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,21 +1,51 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 import { mainConfig } from './config/main.config';
-
-const { frontendURI, apiPrefix, apiPort } = mainConfig;
+//frontendURI,
+const { apiPrefix, apiPort } = mainConfig;
+declare const module: any;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.setGlobalPrefix(apiPrefix);
   app.enableCors({
-    origin: frontendURI,
+    origin: true,
+    // origin: frontendURI,
     credentials: true,
   });
-  app.enableShutdownHooks();
+  app.use(helmet());
+  // app.enableShutdownHooks();
   app.use(cookieParser());
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      // forbidNonWhitelisted: true,
+      // whitelist: true,
+    }),
+  );
+  // app.useWebSocketAdapter(new WsAdapter(app));
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS test app')
+    .setDescription('The test app API description')
+    .setVersion('1.0')
+    .addTag('boilerplate')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(apiPort);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
