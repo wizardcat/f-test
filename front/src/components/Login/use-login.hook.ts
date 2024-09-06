c8d488078800d6d091b5b202be5c36b042224b23
Roman Havrylko <groman2000@gmail.com>
2024-09-06 18:43:34 +0300

    feat: updated login hook

--- front/src/components/Login/use-login.hook.ts
+++ front/src/components/Login/use-login.hook.ts
@@ -1,8 +1,9 @@
-import { useAuth } from '@app/providers/auth.provider';
+import { useAuthContext } from '@app/hooks/use-auth-context.hook';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
+import { Bounce, toast } from 'react-toastify';
 import { z } from 'zod';
 
 const LoginSchema = z.object({
@@ -21,18 +22,28 @@ export const useLogin = () => {
     resolver: zodResolver(LoginSchema),
   });
 
-  const { user, login } = useAuth();
+  const { isAuthenticated, authLogin } = useAuthContext();
   const navigate = useNavigate();
 
   const onSubmit = async (data: LoginSchemaType) => {
-    await login(data.email, data.password);
+    authLogin(data.email, data.password);
   };
 
   useEffect(() => {
-    if (user) {
-      navigate('/user');
-    }
-  }, [user, navigate]);
+    if (isAuthenticated) navigate('/user');
+
+    toast.error('Login failed!', {
+      position: 'bottom-center',
+      autoClose: 1000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: 'light',
+      transition: Bounce,
+    });
+  }, [isAuthenticated, navigate]);
 
   return {
     register,
